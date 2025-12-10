e1394d9796c7638429e6663bae0dfb51946f850b
Tyson <techno2main@gmail.com>
2025-12-10 18:04:29 +0100

    feat(ui): ajout DialogDescription clavier numerique

--- src/components/ui/numeric-keyboard.tsx
+++ src/components/ui/numeric-keyboard.tsx
@@ -4,6 +4,7 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Plus, Minus } from "lucide-react";
